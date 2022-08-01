f7bd711c8e8fdf6b9cda04851d4783986e0a712b
thanhhd <thanhhd@aladintech.co>
2022-08-01 09:04:35 +0700

    itnit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+// module.exports = () => {
+//   // const rewrites = () => {
+//   //   return [
+//   //     {
+//   //       source: "api/testApi",
+//   //       destination: "http://192.168.1.22:8787/v5/home",
+//   //     },
+//   //   ];
+//   // };
+//   // return {
+//   //   rewrites,
+//   // };
+// };
---
+++ pages/api/testApi.js
@@ -0,0 +1,12 @@
+// import axios from "axios";
+
+// export default async function handler(req, res) {
+//   try {
+//     const data = await axios.get("http://192.168.1.22:8787/v5/home");
+//     console.log(data.data)
+//     res.status(200).json(data.data);
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(error.status || 500).end(error.message);
+//   }
+// }
